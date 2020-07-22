83b60e24b50cfa38c41b68b7596860ebb8896f26
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-07-23 00:27:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+app= express()
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    
+    res.render(path.join(__dirname,'/public/views/index.ejs'));
+});
+
+app.listen(3000);
