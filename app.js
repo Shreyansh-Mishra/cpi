b401b9ab20cd5cba5e76cf383d237215f50099a2
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-07-23 10:28:30 +0530

    added navbar and title image

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-app= express()
+var app= express();
 let ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
