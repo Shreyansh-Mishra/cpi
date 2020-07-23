c8c8a48129b54c7e8317d813ca18a5bb1efac8d1
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-07-23 16:21:13 +0530

    Add Doors on Homepage

---
+++ public/views/index.js
@@ -0,0 +1,12 @@
+console.log('hi')
+document.getElementById('changes8').onclick(function(){
+    changeIt('s8');
+});
+document.getElementById('changes7').onclick(function(){
+    changeIt('s7');
+});
+document.getElementById('changes4').onclick(function(){
+    changeIt('s4');
+});
+
+
