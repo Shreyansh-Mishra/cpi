f9d258f3ca269730bd2959f331859e25a585e712
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-07-24 19:27:32 +0530

    Add DB Methods (Remove/Update/Show)

--- app.js
+++ app.js
@@ -1,46 +1,78 @@
 var express = require('express');
 var path = require('path');
-var app= express();
+var app = express();
 var router = express.Router();
 let ejs = require('ejs');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('json spaces', 2);
+
 
 const User = require('./models/user.js');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-    res.render(path.join(__dirname,'/public/views/index/index.ejs'));
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname, '/public/views/index/index.ejs'));
 });
 
-app.get('/form',function(req,res) {
-    res.render(path.join(__dirname,'/public/views/form/form.ejs'));
+app.get('/form', function (req, res) {
+    res.render(path.join(__dirname, '/public/views/form/form.ejs'));
 })
 
-app.get('/showdb', async function(req,res) {
-    db = await User.find({'bio': 'pro at csgo'})
-    res.send(db)
+app.get('/showdb', async function (req, res) {
+    db = await User.find({})
+    res.json(db)
+})
+
+app.get('/updatedb', async function (req, res) {
+
+    const filter = {};
+    const update = {};
+    if(filter==={}){
+        res.send('no filter was provided.')
+    }
+    else {
+    let doc = await User.findOneAndUpdate(filter, update, {
+        useFindAndModify: false,
+        new: true
+    });
+    db = await User.find({})
+    res.json(db)
+}
+})
+
+app.get('/deletedb', async function(req, res) {
+    arg = {}
+    if (arg==={}) {
+        res.send('delete arg cannot be empty');
+    }
+    else {
+        db = await User.find(arg).remove();
+    }
 })
 
 app.post('/submitform', (req, res) => {
     var post = new User(req.body);
     console.log(req.body)
-    post.save(function(err, user) {
-        if(err) console.log(err);
+    post.save(function (err, user) {
+        if (err) console.log(err);
         return res.send("Success! Your post has been saved.");
     });
 });
 
-app.get('/profile/:id', async function(req,res){
+app.get('/profile/:id', async function (req, res) {
     var users = await User.find({});
-    var userprofile = users[parseInt(req.params.id)-1];
-    if(userprofile){
-    res.render(path.join(__dirname, '/public/profile/profile.ejs'), {profile: userprofile});
-    }
-    else{
+    var userprofile = users[parseInt(req.params.id) - 1];
+    if (userprofile) {
+        res.render(path.join(__dirname, '/public/views/profile/profile.ejs'), {
+            profile: userprofile
+        });
+    } else {
         res.send('User Not Found');
     }
 })
