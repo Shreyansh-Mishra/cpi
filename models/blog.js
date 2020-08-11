6e9f224c02c4788b4fb3a91af663e510e87315ab
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-08-11 22:42:37 +0530

    Add new model for blog section

---
+++ models/blog.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('dotenv').config();
+mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const blog = new Schema({
+    post: {type: String, required: true},
+    by: {type: String, required: true},
+    date: {type: Date, default: Date.now},
+    id: {type: String}
+});
+
+var Blog = mongoose.model('blogtest', blog);
+module.exports = Blog;
